b648be3d15c0273053968f052af3bf54da4c7f95
tryingMyHardest <121779137+tryingMyHardest@users.noreply.github.com>
2023-01-04 12:58:48 -0500

    Updates to Code editor
    Making updates and testing code editor options in projects.php file.

--- projects.php
+++ projects.php
@@ -1,31 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Alex Doran | Projects</title>
-<style type="text/css" media="screen">
-    #editor { 
-        position: absolute;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-    }
-</style>
+    <meta charset="UTF-8">
+    <title>Alex Doran | Projects</title>
+    <link rel="stylesheet" href="/cms/styles.css">
+    
+    <script src="/cms/src-noconflict/ace.js" type="text/javascript" charset="utf-8">
+
+        function createEditor(){
+            var editor = ace.edit("editor");
+            editor.setTheme("ace/theme/monokai");
+            editor.session.setMode("ace/mode/java");
+        }
+        var editor = ace.edit("editor");
+            editor.setTheme("ace/theme/monokai");
+            editor.session.setMode("ace/mode/java");
+    </script>
 </head>
+
 <body>
 
-<h3>This is other stuff</h3>
-
-<div id="editor">
-<?php echo readfile("LinkedList.java"); ?>
-</div>
     
-<script src="/cms/src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
-<script>
-    var editor = ace.edit("editor");
-    editor.setTheme("ace/theme/monokai");
-    editor.session.setMode("ace/mode/java");
-</script>
+    <section id="project">
+        <h3>Project 1</h3>
+        <input id="CE_tab" type="button" value="Js" onclick="editor.setSession(js)">
+        <input id="CE_tab" type="button" value="CSS" onclick="editor.setSession(css)">
+        <div id="editor">
+        </div>
+    </section>
+
+    <section id="project">
+        <h3>Project 2</h3>
+        <div id="editor">
+            <?= readfile("Stack.java") ?>
+        </div>
+    </section>
+
+    <script>
+        var editor = ace.edit("editor");
+        editor.setTheme("ace/theme/monokai");
+        editor.session.setMode("ace/mode/java");
+        editor.session.setUseSoftTabs(true);   
+
+        var js = ace.createEditSession("test for js");
+        var css = ace.createEditSession(["test for css", "ahh", "peepee"]);
+        editor.setSession(css);
+        
+    </script>
+
 </body>
 </html>
 
