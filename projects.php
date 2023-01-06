065fd33f5ce9315d7a1deb7bb03fa85e098de043
tryingMyHardest <121779137+tryingMyHardest@users.noreply.github.com>
2023-01-05 23:23:29 -0500

    Code Editor Update
    Updating how the Ace code editor is implemented in the site. Found a way to dynamically create and destroy editors to reduce memory used. Will implement in projects.php soon.

--- projects.php
+++ projects.php
@@ -8,18 +8,49 @@
 </head>
 
 <body>
-  
     <section class="project">
-        <h3>Project 1</h3>
+    <h3>Project 1</h3>
+    <button class="expandProject" onclick="changeProject(event, 'project1')">See More &#8600</button>
+    <div id="project1" class="codeContent">
         <div class="tab">
-            <button class="codeTab" onclick="changeTab(event, 'editor3')">HTML</button>
+            <button id="defaultOpen" class="codeTab" onclick="changeTab(event, 'editor3')">HTML</button>
             <button class="codeTab" onclick="changeTab(event, 'editor1')">JS</button>
             <button class="codeTab" onclick="changeTab(event, 'editor2')">CSS</button>
+            <span class="closeProject" onclick="this.parentElement.parentElement.style.display='none'">&#8598</span>
         </div>
         <div id="editor1" class="editor"><?= readfile("firstHTMLpage/javascript.js")?></div>
         <div id="editor2" class="editor"><?= readfile("firstHTMLpage/styles.css")?></div>
-        <div id="editor3" class="editor"><?= htmlspecialchars(readfile("firstHTMLpage/index.html"))?><div>
-    </section>
+        <div id="editor3" class="editor"><?= readfile("firstHTMLpage/index.html")?></div>
+</div>
+</section>
+
+<section class="project">
+    <h3>Project 2</h3>
+    <button class="expandProject" onclick="changeProject(event, 'project2')">See More</button>
+    <div id="project2" class="codeContent">
+        <div data-pym-src='https://www.jdoodle.com/embed/v0/5HlA?arg=0' data-language="java"
+        data-version-index="4" data-libs="mavenlib1, mavenlib2"><?= readfile("javeFiles/LinkedList.java") ?></div>
+        <script src="https://www.jdoodle.com/assets/jdoodle-pym.min.js" type="text/javascript"></script>
+    </div>
+</section>
+
+<section class="project">
+    <h3>Project 3</h3>
+    <button class="expandProject" onclick="changeProject(event, 'project3')">See More</button>
+    <div id="project3" class="codeContent">
+    <div class="tab">
+            <button id="defaultOpen" class="codeTab" onclick="changeTab(event, 'editor4')">HTML</button>
+            <button class="codeTab" onclick="changeTab(event, 'editor1')">JS</button>
+            <button class="codeTab" onclick="changeTab(event, 'editor2')">CSS</button>
+            <span class="closeProject" onclick="this.parentElement.parentElement.style.display='none'">&#8598</span>
+        </div>
+        <div id="editor4" class="editor"><?= readfile("firstHTMLpage/styles.css")?></div>
+    </div>
+</section>
+
+Test
+<input type="button" value="click me" onclick="createEditor()">
+<div id="editor5" class="editor">Test</div>
 
     <script>
         var editor1 = ace.edit("editor1");
@@ -31,6 +62,12 @@
         var editor3 = ace.edit("editor3");
         editor3.session.setMode("ace/mode/HTML");
         editor3.setTheme("ace/theme/monokai");
+        var editor4 = ace.edit("editor4");
+            editor4.session.setMode("ace/mode/css");
+            editor4.setTheme("ace/theme/monokai");
+            editor4.resize();
+
+        document.getElementById("defaultOpen").click();
 
         function changeTab(evt, editorNum){
             // Declare all variables
@@ -51,6 +88,23 @@
             // Show the current tab, and add an "active" class to the button that opened the tab
             document.getElementById(editorNum).style.display = "block";
             evt.currentTarget.className += " active";
+        }
+
+        function changeProject(evt, projNum){
+            var i, codeContent;
+
+            codeContent = document.getElementsByClassName("codeContent");
+            for(i = 0; i < codeContent.length; i++){
+                codeContent[i].style.display = "none";
+            }
+
+            document.getElementById(projNum).style.display = "block";
+        }
+
+        function createEditor(){
+            let editor = ace.edit("editor5");
+            editor.session.setMode("ace/mode/java");
+            editor.setTheme("ace/theme/monokai");
         }
     </script>
 </body>
