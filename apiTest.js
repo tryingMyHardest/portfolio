a968ba1add5fb777b761d3b4c6a818c26d048c1e
tryingMyHardest <121779137+tryingMyHardest@users.noreply.github.com>
2023-01-16 08:44:15 -0500

    Update apiTest.js

---
+++ apiTest.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const options = {
+  method: 'POST',
+  url: 'https://judge0-ce.p.rapidapi.com/submissions',
+  params: {base64_encoded: 'true', fields: '*'},
+  headers: {
+    'content-type': 'application/json',
+    'Content-Type': 'application/json',
+    'X-RapidAPI-Key': '0f362b5a43msh406f5870c7e287ep11dc52jsnc6ef63ed4990',
+    'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+  },
+  data: '{"language_id":52,"source_code":"I2luY2x1ZGUgPHN0ZGlvLmg+CgppbnQgbWFpbih2b2lkKSB7CiAgY2hhciBuYW1lWzEwXTsKICBzY2FuZigiJXMiLCBuYW1lKTsKICBwcmludGYoImhlbGxvLCAlc1xuIiwgbmFtZSk7CiAgcmV0dXJuIDA7Cn0=","stdin":"SnVkZ2Uw"}'
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
